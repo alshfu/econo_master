70192b8bd851fe3ab4431898f816ebb558fae2d8
alsh <alshfu@gmail.com>
2023-07-21 00:44:04 +0200

    new version

--- lib/economaster.dart
+++ lib/economaster.dart
@@ -4,10 +4,19 @@ import 'package:econo_master/vat_calculator/vat_calculator.dart';
 import 'package:econo_master/about.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:econo_master/localization.dart';
+import 'package:econo_master/vat_calculator/vat_calculation.dart';  // Import VATCalculation
+import 'package:econo_master/vat_calculator/calculation_history_table.dart';  // Import CalculationHistoryTable
 
-
-class EconoMaster extends StatelessWidget {
+class EconoMaster extends StatefulWidget {
   const EconoMaster({Key? key}) : super(key: key);
+
+  @override
+  _EconoMasterState createState() => _EconoMasterState();
+}
+
+class _EconoMasterState extends State<EconoMaster> {
+  final calculations = ValueNotifier<List<VATCalculation>>([]);
+
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +31,7 @@ class EconoMaster extends StatelessWidget {
         const Locale('ru', ''),
       ],
       home: DefaultTabController(
-        length: 3,
+        length: 4,  // Increase the length to 4
         child: Scaffold(
           appBar: AppBar(
             title: const Text('EconoMaster'),
@@ -30,7 +39,8 @@ class EconoMaster extends StatelessWidget {
               tabs: [
                 Tab(text: 'VAT Calculator'),
                 Tab(text: 'RUT Calculator'),
-                Tab(text: 'About')
+                Tab(text: 'About'),
+                Tab(text: 'History'),  // Add a new tab
               ],
             ),
           ),
@@ -41,6 +51,7 @@ class EconoMaster extends StatelessWidget {
                   VATCalculator(),
                   RUTCalculator(),
                   const About(),
+                  CalculationHistoryTable(calculations),  // Add a new tab view
                 ],
               ),
               Positioned(
@@ -68,7 +79,6 @@ class EconoMaster extends StatelessWidget {
     );
   }
 }
-
 
 void main() {
   runApp(const EconoMaster());
--- lib/vat_calculator/calculation_history_table.dart
+++ lib/vat_calculator/calculation_history_table.dart
@@ -3,47 +3,59 @@ import 'package:econo_master/localization.dart';
 
 import 'vat_calculation.dart'; // Update the import
 
-class CalculationHistoryTable extends StatelessWidget {
+class CalculationHistoryTable extends StatefulWidget {
   final ValueNotifier<List<VATCalculation>> calculations;
 
   CalculationHistoryTable(this.calculations);
 
   @override
+  _CalculationHistoryTableState createState() =>
+      _CalculationHistoryTableState();
+}
+
+class _CalculationHistoryTableState extends State<CalculationHistoryTable>
+    with AutomaticKeepAliveClientMixin {
+  @override
+  bool get wantKeepAlive => true; // This ensures that the state is kept alive
+
+  @override
   Widget build(BuildContext context) {
+    super.build(context);
     final loc = AppLocalizations.of(context);
 
     return ValueListenableBuilder<List<VATCalculation>>(
-      valueListenable: calculations,
+      valueListenable: widget.calculations,
       builder: (context, calculations, child) {
-        return DataTable(
-          columns: [
-            DataColumn(label: Text('ID')),
-            DataColumn(label: Text(loc.translate('price_before'))),
-            DataColumn(label: Text(loc.translate('price_after'))),
-            DataColumn(label: Text(loc.translate('vat'))),
-            DataColumn(label: Text(loc.translate('vat_rate'))),
-            DataColumn(label: Text(loc.translate('vat_type'))),
-          ],
-          rows: calculations.map((calculation) {
-            return DataRow(
-              color: MaterialStateProperty.resolveWith<Color>(
-                      (Set<MaterialState> states) {
-                    if (calculation.taxType == 'Incoming VAT')
-                      return Colors.green.withOpacity(0.3);
-                    if (calculation.taxType == 'Outgoing VAT')
-                      return Colors.red.withOpacity(0.3);
-                    return Colors.blue.withOpacity(0.3);
-                  }),
-              cells: [
-                DataCell(Text(calculation.id.toString())),
-                DataCell(Text(calculation.priceBefore.toStringAsFixed(2))),
-                DataCell(Text(calculation.priceAfter.toStringAsFixed(2))),
-                DataCell(Text(calculation.vat.toStringAsFixed(2))),
-                DataCell(Text(calculation.vatRate.toStringAsFixed(2))),
-                DataCell(Text(loc.translate(calculation.taxType))),
-              ],
-            );
-          }).toList(),
+        return SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: DataTable(
+            columns: [
+              DataColumn(label: Text('ID')),
+              DataColumn(label: Text(loc.translate('price_before'))),
+              DataColumn(label: Text(loc.translate('price_after'))),
+              DataColumn(label: Text(loc.translate('vat'))),
+              DataColumn(label: Text(loc.translate('vat_rate'))),
+            ],
+            rows: calculations.map((calculation) {
+              return DataRow(
+                color: MaterialStateProperty.resolveWith<Color>(
+                        (Set<MaterialState> states) {
+                      if (calculation.taxType == 'Incoming VAT')
+                        return Colors.green.withOpacity(0.3);
+                      if (calculation.taxType == 'Outgoing VAT')
+                        return Colors.red.withOpacity(0.3);
+                      return Colors.blue.withOpacity(0.3);
+                    }),
+                cells: [
+                  DataCell(Text(calculation.id.toString())),
+                  DataCell(Text(calculation.priceBefore.toStringAsFixed(2))),
+                  DataCell(Text(calculation.priceAfter.toStringAsFixed(2))),
+                  DataCell(Text(calculation.vat.toStringAsFixed(2))),
+                  DataCell(Text(calculation.vatRate.toStringAsFixed(2))),
+                ],
+              );
+            }).toList(),
+          ),
         );
       },
     );
